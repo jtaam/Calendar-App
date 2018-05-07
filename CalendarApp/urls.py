512414da3b318f530097883664770ada2577c9fb
jtaam <tamjid.mdh@gmail.com>
2018-05-07 08:26:58 +0600

    details page

--- CalendarApp/urls.py
+++ CalendarApp/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from  myapp import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('entry/<int:pk>', views.details, name='details'),
     path('admin/', admin.site.urls),
 ]
 """CalendarApp URL Configuration
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Entry
 
 
 def index(request):
-    return render(request, 'myapp/index.html')
+    entries = Entry.objects.all()
+    return render(request, 'myapp/index.html', {'entries': entries})
+
+
+def details(request, pk):
+    entry = Entry.objects.get(id=pk)
+    return render(request, 'myapp/details.html',  {'entry': entry})
