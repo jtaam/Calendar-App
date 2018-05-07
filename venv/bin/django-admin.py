5cb44538757dbecc18f4a0aaa953d6a225ace26a
jtaam <tamjid.mdh@gmail.com>
2018-05-07 07:25:49 +0600

    first templating

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/jtam/Documents/Projects/Python/Django/CalendarApp/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
