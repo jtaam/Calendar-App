180103abfc33c9fc0c2ef54f7e8187c93b18d9aa
jtaam <tamjid.mdh@gmail.com>
2018-05-07 16:33:21 +0600

    delete entry

--- CalendarApp/urls.py
+++ CalendarApp/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from  myapp import views
+from myapp import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('entry/<int:pk>', views.details, name='details'),
+    path('entry/add', views.add, name='add'),
+    path('entry/delete/<int:pk>', views.delete, name='delete'),
     path('admin/', admin.site.urls),
 ]
 """CalendarApp URL Configuration
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Entry
-
+from .forms import EntryForm
+from django.http import HttpResponseRedirect
 
 def index(request):
     entries = Entry.objects.all()
@@ -10,3 +11,32 @@ def index(request):
 def details(request, pk):
     entry = Entry.objects.get(id=pk)
     return render(request, 'myapp/details.html',  {'entry': entry})
+
+
+def add(request):
+    if request.method == 'POST':
+        form = EntryForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            date = form.cleaned_data['date']
+            description = form.cleaned_data['description']
+
+            Entry.objects.create(
+                name=name,
+                date=date,
+                description=description
+            ).save()
+
+            return HttpResponseRedirect('/')
+    else:
+        form = EntryForm()
+
+    return render(request, 'myapp/form.html', {'form':form})
+
+
+def delete(request, pk):
+    if request.method == 'DELETE':
+        entry = get_object_or_404(Entry, pk=pk)
+        entry.delete()
+
+        return HttpResponseRedirect('/')
